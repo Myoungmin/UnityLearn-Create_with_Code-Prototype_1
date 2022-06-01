3d480a29b47ecf46ccc50c43b44229885b437901
Myoungmin <miung222@gmail.com>
2022-06-01 22:03:41 +0900

    1.Allow the vehicle to move left/right

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +19,6 @@ public class PlayerController : MonoBehaviour
         //We'll move the vehicle forward
         //Time.deltaTime을 이용하여 차량의 이동속도를 보정
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
