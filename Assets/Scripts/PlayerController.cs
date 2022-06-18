56c7fa06a3a620da072dbaf1aebf8ce29c261d7e
Myoungmin <miung222@gmail.com>
2022-06-18 22:57:06 +0900

    1.Make the vehicle use forces

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 20.0f;
+    //[SerializeField] private float speed = 20.0f;
+    [SerializeField] private float horsePower = 0;
     private const float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+    private Rigidbody playerRb;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -18,8 +25,9 @@ public class PlayerController : MonoBehaviour
 
         //Time.deltaTime을 이용하여 차량의 이동속도를 보정
         // Moves the cat forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        playerRb.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
+
         //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
         
         // Rotates the car based on horizontal input
