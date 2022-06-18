67d613b7e74e85ed160962a22402e40e4000f2ae
Myoungmin <miung222@gmail.com>
2022-06-18 22:32:13 +0900

    2.Unity Event Functions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,14 +9,8 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float forwardInput;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // This is where we get Player input
         horizontalInput = Input.GetAxis("Horizontal");
